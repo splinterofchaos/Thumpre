c288601b8ea8f3cdcc93fa97a6a62db6d53af60e
Scott Prager <hakusa@gmail.com>
2012-03-14 06:23:32 -0400

    Refactoring; print_map, _log, _inventory.

--- rogue.cpp
+++ rogue.cpp
@@ -13,6 +13,11 @@
 
 #include <ncurses.h>
 
+// This just makes using std::for_each a little easier to write/read.
+#define RNG( container ) container.begin(), container.end()
+#define FOR_EACH( container, value, block ) \
+    std::for_each( RNG(container), [&]( const value ){ block; } )
+
 // Produces a random, non-wall, non-occupied position.
 // It asserts that a free space exists and never exits otherwise.
 Vec random_position()
@@ -31,6 +36,62 @@ Vec random_position()
     }
 }
 
+bool read_map()
+{
+    FILE* mapgen = popen( "./mapgen 20x15", "r" ); 
+    if( not mapgen )
+        return false;
+
+    char row[ 200 ];
+    while( fscanf(mapgen, "%s", row) != EOF )
+        map.push_back( row );
+    pclose( mapgen );
+
+    return true;
+}
+
+void print_map( Vec where )
+{
+    // Instead of painting the map, then items, then actors onto the  screen,
+    // just copy the map to a buffer, paint everything to it,  and paint it to
+    // the screen. This means that buffer can be put  anywhere on screen
+    // without making sure everything's being  painted with the same offset.
+    Map toScreen = map;
+    FOR_EACH( items, Item& i, toScreen[i.pos.y][i.pos.x] = i.image );
+    FOR_EACH( npcs,  Npc&  n, toScreen[n.pos.y][n.pos.x] = n.image );
+
+    toScreen[player.pos.y][player.pos.x] = player.image;
+
+    unsigned int row = 0;
+    FOR_EACH ( 
+        toScreen, std::string& line, 
+        mvprintw(where.y + row++, where.x, line.c_str())
+    );
+}
+
+void print_log( Vec where )
+{
+    unsigned int row = 0;
+    FOR_EACH ( 
+        logger, std::string& msg, 
+        mvprintw( where.y + row++, where.x, msg.c_str() ) 
+    );
+    logger.clear();
+}
+
+void print_inventory( Vec where )
+{
+    // Print the inventory.
+    mvprintw( where.y++, where.x, "You have:" );
+    if( player.inventory.size() )
+        FOR_EACH ( 
+            player.inventory, Item& i, 
+            mvprintw( where.y++, where.x+2, i.name.c_str() ) 
+        );
+    else
+        mvprintw( where.y, where.x+2, "Nothing." );
+}
+
 int main( int argc, char** argv )
 {
     initscr();
@@ -41,16 +102,7 @@ int main( int argc, char** argv )
 
     std::srand( std::time(0) );
 
-    { // Read in the map from mapgen, the generic map generator.
-        FILE* mapgen = popen( "./mapgen 20x15", "r" ); 
-        if( not mapgen )
-            return 1;
-
-        char row[ 200 ];
-        while( fscanf(mapgen, "%s", row) != EOF )
-            map.push_back( row );
-        pclose( mapgen );
-    }
+    read_map();
 
     player.pos = random_position();
 
@@ -75,48 +127,12 @@ int main( int argc, char** argv )
         if( itemHere != items.end() )
             logger.push_back( "Your foot hits a " + itemHere->name + "." );
 
-        #define RNG( container ) container.begin(), container.end()
-        #define FOR_EACH( container, value, block ) \
-            std::for_each( RNG(container), [&]( const value ){ block; } )
-
         const int MAP_TOP = 5;
         const int MAP_BOTTOM = MAP_TOP + map.size();
 
-        { // Draw the map.
-            // Instead of painting the map, then items, then actors onto the
-            // screen, just copy the map to a buffer, paint everything to it,
-            // and paint it to the screen. This means that buffer can be put
-            // anywhere on screen without making sure everything's being
-            // painted with the same offset.
-            Map toScreen = map;
-            FOR_EACH( items, Item& i, toScreen[i.pos.y][i.pos.x] = i.image );
-            FOR_EACH( npcs,  Npc&  n, toScreen[n.pos.y][n.pos.x] = n.image );
-
-            toScreen[player.pos.y][player.pos.x] = player.image;
-
-            unsigned int row = 0;
-            FOR_EACH ( 
-                toScreen, std::string& line, 
-                mvprintw(MAP_TOP + row++, 2, line.c_str())
-            );
-        }
-
-        unsigned int row = 0;
-        FOR_EACH( logger, std::string& msg, mvprintw(1 + row++, 1, msg.c_str()) );
-        logger.clear();
-
-        // Print the inventory.
-        row = MAP_BOTTOM + 2;
-        mvprintw( row++, 4, "You have:" );
-        if( player.inventory.size() )
-        {
-            FOR_EACH ( 
-                player.inventory, Item& i, 
-                mvprintw( row++, 6, i.name.c_str() ) 
-            );
-        }
-        else
-            mvprintw( row, 6, "Nothing." );
+        print_map( Vec(2, MAP_TOP) );
+        print_log( Vec(1, 0) );
+        print_inventory( Vec(4, MAP_BOTTOM+2) );
 
         refresh(); 
 
