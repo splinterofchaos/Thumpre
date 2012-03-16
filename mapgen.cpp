1a5ce74df4a8bca8ea32f4623c5d7ed122041a8b
Scott Prager <hakusa@gmail.com>
2012-03-16 08:05:21 -0400

    Simplified mapgen.cpp.

--- mapgen.cpp
+++ mapgen.cpp
@@ -5,18 +5,70 @@
 
 #include <time.h> // For time() to generate random seed.
 
+#include <string>
+#include <vector>
+#include <algorithm>
+
 typedef const char* const literal;
 
 struct Vec { int x; int y; };
+struct Rect { int left; int right; int up; int down; };
+
+typedef std::vector< std::string > Map;
+Map map;
+int width;
+int height;
 
 int random( int till, int from=0 )
 {
     return rand() % (till-from) + from;
 }
 
+void make_map( char* argDimensions )
+{
+    sscanf( argDimensions, "%dx%d", &width, &height );
+
+    map.reserve( height );
+
+    int i = height;
+    while( i-- )
+        map.push_back( std::string(width, '#') );
+}
+
+bool add_room( const Rect& area )
+{
+    // Make sure no room exists in this area.
+    for( int y=area.up; y<area.down; y++ )
+        for( int x=area.left; x<area.right; x++ )
+            if( map[y][x] == '.' )
+                return false;
+
+    for( int y=area.up; y<area.down; y++ )
+        for( int x=area.left; x<area.right; x++ )
+            map[y][x] = '.';
+
+    return true;
+}
+
+Rect random_rect( int minW, int minH )
+{
+    Rect ret;
+    
+    ret.left = random( width  - minW - 1, 1 );
+    ret.up   = random( height - minH - 1, 1 );
+
+    ret.right = random( width  - 1, ret.left + minW );
+    ret.down  = random( height - 1, ret.up   + minH);
+
+    if( ret.left > ret.right or ret.up > ret.down )
+        printf( "ERROR" );
+
+    return ret;
+}
+
 int main( int argc, char** argv )
 {
-    literal usage = "mapgen ([X]x[Y])\n"
+    literal usage = "mapgen [X]x[Y]\n"
                     "   Where X and Y represent the size of the output map.";
 
     if( argc < 2 )
@@ -25,38 +77,13 @@ int main( int argc, char** argv )
         exit( 1 );
     }
 
-    int width, height;
-    sscanf( argv[1], "%dx%d", &width, &height );
-
-    char** map = new char*[ height ];
-    for( int i=0; i < height; i++ )
-    {
-        map[i] = new char[ width + 1];
-        map[i][width] = '\0';
-    }
-
-    for( int y=0; y < height; y++ )
-    {
-        for( int x=0; x < width; x++ )
-            map[y][x] = '#';
-    }
+    make_map( argv[1] );
 
     // For now, just create a room at a random position.
     srand(time(0));
-    int startX = random( width/2,  1 );
-    int startY = random( height/2, 1 );
-    int endX = random( width -1,  startX + 3 );
-    int endY = random( height-1, startY + 3 );
 
-    for( int y=startY; y <= endY; y++ )
-        for( int x=startX; x <= endX; x++ )
-            map[y][x] = '.';
-
+    add_room( random_rect(3,3) );
 
     for( int y=0; y < height; y++ )
-        printf( "%s\n", map[y] );
-
-    for( int y=0; y < height; y++ )
-        delete [] map[y];
-    delete [] map;
+        printf( "%s\n", map[y].c_str() );
 }
